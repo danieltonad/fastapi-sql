ab987882747f7198398c748fe05c15f94904771a
danieltonad <soladan2000@gmail.com>
2024-03-04 11:52:29 -0900

    bug: py <-> sql connection

---
+++ config/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+URL_DATABASE = 'mysql+pymysql://root:@localhost:3306/blog'
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# meta = MetaData()
+
+# conn = engine.connect()
+
+Base = declarative_base()
--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import Annotated
+from models import model
+from config.database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI(title="Sql Test")
+model.Base.metadata.create_all(bind=engine)
 
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    user_id: int
+
+class UserBase(BaseModel):
+    username: str
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+         yield db
+    finally:
+        db.close()
+        
+db_dependency = Annotated[Session, Depends(get_db)]   
+
+
+    
 @app.get('/')
 async def root():
     return "SWL"
+
+@app.post("/users/", status_code=status.HTTP_201_CREATED)
+async def create_user_route(user: UserBase, db: db_dependency):
+    db_user = model.User(**user)
+    db.add(db_user)
+    db.commit()
