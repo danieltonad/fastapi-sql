175788b4bf5e07f86fc9951af9b1467a86b4e8da
danieltonad <soladan2000@gmail.com>
2024-03-17 12:05:31 -0900

    didnt get the Update shii

--- main.py
+++ main.py
@@ -38,3 +38,41 @@ async def create_user_route(user: UserBase, db: db_dependency):
     db_user = model.User(**user.dict())
     db.add(db_user)
     db.commit()
+    
+@app.get("/users")
+async def get_user(db: db_dependency):
+    user = db.query(model.User).all()
+    return user
+
+@app.get("/user/{user_id}")
+async def read_user(user_id: int, db: db_dependency):
+    user = db.query(model.User).filter(model.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+    
+    return user
+    
+@app.put("/user/{user_id}")
+async def update_user(user_id: int, user: UserBase ,db: db_dependency):
+    user_db = db.query(model.User).filter(model.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+    print(user)
+     # Update user data
+    # for key, value in user.dict(exclude_unset=True).items():
+    #     setattr(user, key, value)
+
+    db.commit()
+    # db.refresh(user)
+    # db.add()
+    return user
+
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: int , db: db_dependency):
+    user = db.query(model.User).filter(model.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+    
+    db.delete(user)
+    db.commit()
+    return "user deleted"
